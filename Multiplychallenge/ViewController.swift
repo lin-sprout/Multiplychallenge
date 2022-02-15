215256dbd430c61900d251dee1de69c3f0d1bf08
Lin LaMonte <penelopelamonte@gmail.com>
2022-02-15 14:19:01 -0700

    Started

--- Multiplychallenge/ViewController.swift
+++ Multiplychallenge/ViewController.swift
@@ -3,17 +3,35 @@
 //  Multiplychallenge
 //
 //  Created by Lin LaMonte on 2/15/22.
+//  fueled by Lemon Demon: Two Trucks
 //
+// how to print integer in string? productLabel.text = "the product is \(productNumber)"
 
 import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var insrtText1: UITextField!
+    
+    @IBOutlet weak var addText2: UITextField!
+    
+
+    
+    
+    @IBOutlet weak var productDisplayLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func calculatedProduct(_ sender: Any) {
+    }
+    
+    
 
 }
 
