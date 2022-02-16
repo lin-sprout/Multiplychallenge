c408abacdfd559d7941af14743e88b24dec5803d
Lin LaMonte <penelopelamonte@gmail.com>
2022-02-16 14:13:36 -0700

    I got a little bit further

--- Multiplychallenge/ViewController.swift
+++ Multiplychallenge/ViewController.swift
@@ -16,7 +16,22 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var addText2: UITextField!
     
-
+//these r the variables for the boxes
+    var initialInputBoxA = 0
+    var initialInputBoxB = 0
+    
+    var dataCollectA: String = ""
+    var dataCollectB: String = ""
+    
+  
+    
+    @IBOutlet weak var imgViewA: UIImageView!
+    
+    @IBOutlet weak var imgViewB: UIImageView!
+    
+    @IBOutlet weak var imgViewC: UIImageView!
+    
+    
     
     
     @IBOutlet weak var productDisplayLabel: UILabel!
@@ -25,10 +40,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //imgViewA.image = UIImage(named: "lonk")
+        //imgViewB.image = UIImage(named: "pikchu")
+        //imgViewC.image = UIImage(named: "kat")
+        
+        
     }
 
     
     @IBAction func calculatedProduct(_ sender: Any) {
+        //double ?? is an optional, it will assign the default value to the right of the ??nif user doesnt enter anything
+        dataCollectA = insrtText1.text ?? ""
+        
+        dataCollectB = addText2.text ?? ""
+        
+        
     }
     
     
